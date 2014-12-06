eab5ebf1667e7640162382cd5bcec1eb42b34c68
masakiishii <masaki.ishii511@gmail.com>
2014-12-06 19:41:01 +0900

    add Problem55.hs

---
+++ ans/54Ato60/Problem55.hs
@@ -0,0 +1,11 @@
+module Problem55 (Tree(..)) where
+
+data Tree a = Empty | Branch a (Tree a) (Tree a)
+     deriving (Show, Eq)
+
+cbalTree :: Int -> [Tree Char]
+cbalTree 0 = [Empty]
+cbalTree n = let (q, r) = (n - 1) `quotRem` 2
+                 in [Branch 'x' left right | i <- [q .. q + r],
+                            left  <- cbalTree i,
+                            right <- cbalTree (n - i - 1)]
